3a3cb638dbb0ba711fe683c383585d29088e9031
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-31 18:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const name=document.getElementById("username");
-const password=document.getElementById("password");
-const btn=document.getElementById("submit");
-const check=document.getElementById("checkbox");
-const existbtn=document.getElementById("existing");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const btn = document.getElementById("submit");
+const check = document.getElementById("checkbox");
+const existbtn = document.getElementById("existing");
 
 const savedUsername = localStorage.getItem("username");
 const savedPassword = localStorage.getItem("password");
 
-        if (savedUsername===null && savedPassword===null) {
-            existbtn.style.display = "inline-block";
-        }
+if (savedUsername && savedPassword) {
+    existbtn.style.display = "inline-block";
+} else {
+    existbtn.style.display = "none";
+}
 
-btn.addEventListener('click',(e)=>{
-	e.preventDefault();
-	alert(`Logged in as ${name.value}`)
-	if(check.checked){
-		localStorage.setItem("username",name.value);
-		localStorage.setItem("password",password.value);
-		 existbtn.style.display = "inline-block";
-	}else{
-		localStorage.removeItem("username");
-		localStorage.removeItem("password");
-		 existbtn.style.display = "none";
-	}
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    alert(`Logged in as ${usernameInput.value}`);
+
+    if (check.checked) {
+        localStorage.setItem("username", usernameInput.value);
+        localStorage.setItem("password", passwordInput.value);
+
+        existbtn.style.display = "inline-block";
+    } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+
+        existbtn.style.display = "none";
+    }
 });
-existbtn.addEventListener('click',(e)=>{
-	e.preventDefault();
-	const storeddata=localStorage.getItem("username");
-	alert(`Logged in as ${storeddata}`);
+
+existbtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const storedData = localStorage.getItem("username");
+    alert(`Logged in as ${storedData}`);
 });
